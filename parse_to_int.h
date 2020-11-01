3b6c87739a243c8c9221ac8ab27e7b4c7227204b
Ciro Dourado <38660594+ciroDourado@users.noreply.github.com>
2020-11-01 15:35:01 -0300

    Create parse_to_int.h

---
+++ parse_to_int.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+    // funções nativas do windows
+    #define clear_terminal() system("cls")
+    #define pause_terminal() system("pause")
+#else
+    // funções nativas de sistemas unix-like
+    #define clear_terminal() system("clear")
+    #define pause_terminal() printf("Enter para continuar\n"); getchar()
+#endif
+
+int parse_to_int(char*);
+int parse_to_int_was_successful(char*);
