6b8f8ffd79321d67bae08e7842d3ba1d52ed4f69
Ciro Dourado <38660594+ciroDourado@users.noreply.github.com>
2020-11-01 15:33:11 -0300

    Create parse_to_int.c

---
+++ parse_to_int.c
@@ -0,0 +1,27 @@
+#include "parse_to_int.h"
+
+int parse_to_int(char* string) {
+    if(string == NULL) {
+        printf("Cannot convert from this\n");
+        printf("Returning zero.\n");
+        return 0;
+    } else {
+        return atoi(string);
+    } // end NULL pointer handling
+}
+
+int parse_to_int_was_successful(char* string) {
+    if(string == NULL) {
+        printf("I can't even use this to parse!\n");
+        return 0;
+    } else {
+        if(! atoi(string) && string[0] != '0') {
+            return 0;
+        } else {
+            char test[strlen(string)];
+            sprintf(test, "%d", atoi(string));
+            if(!strcmp(test, string)) return 1;
+            else return 0;
+        }
+    } // end NULL pointer handling
+}
