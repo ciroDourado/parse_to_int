8a043bc9b49905cf1e453e3def021641949080ae
Ciro Dourado <38660594+ciroDourado@users.noreply.github.com>
2020-11-01 15:59:47 -0300

    Update example.c

--- example.c
+++ example.c
@@ -4,13 +4,13 @@ int main() {
     clear_terminal();
 
     char only_numbers[] = "1234";
-    printf("input: %s\n", only_numbers);
+    printf("\ninput: %s", only_numbers);
     if(parse_to_int_was_successful(only_numbers)) {
         printf("\nYour number was: %d\n", parse_to_int(only_numbers));
     }
     
     char chars_before_numbers[] = "hey 42";
-    printf("input: %s\n", chars_before_numbers);
+    printf("\ninput: %s", chars_before_numbers);
     if(parse_to_int_was_successful(chars_before_numbers)) {
         printf("\nYour number was: %d\n", parse_to_int(chars_before_numbers));
     } else {
@@ -21,14 +21,14 @@ int main() {
     // using atoi in the case bellow actually converts your number
     // but this should be a problem, since only numbers should be read
     char numbers_before_chars[] = "42here";
-    printf("input: %s\n", numbers_before_chars);
+    printf("\ninput: %s", numbers_before_chars);
     if(parse_to_int_was_successful(numbers_before_chars)) {
         printf("\nYour number was: %d\n", parse_to_int(numbers_before_chars));
     } else {
         // this part should take care of reading again, or anything else
         printf("\nTry to give a number!\n");
     }
-    printf("\natoi's incredible convertion: %d\n", atoi(numbers_before_chars));
+    printf("atoi's incredible convertion: %d\n", atoi(numbers_before_chars));
     printf("This shouldn't happen!!\n");
     
     // let's imagine you gave a string to atoi with no numbers;
@@ -37,13 +37,13 @@ int main() {
     // able to differentiate an real '0' input instead of an 
     // invalid convertion, in order to handle any possible errors
     char only_chars[] = "i'm a problematic input";
-    printf("input: %s\n", only_chars);
+    printf("\ninput: %s", only_chars);
     if(parse_to_int_was_successful(only_chars)) {
         printf("\nYour number was: %d\n", parse_to_int(only_chars));
     } else {
         printf("\nTry to give a number!\n");
     }
-    printf("\natoi's incredible convertion: %d\n", atoi(only_chars));
+    printf("atoi's incredible convertion: %d\n", atoi(only_chars));
     printf("This shouldn't happen!!\n");
     
     return 0;
